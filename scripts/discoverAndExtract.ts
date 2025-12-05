f6aa50dfd4f350e7d32c644008cf1d886b91fc98
nguyenadamq <nguyenadamq@gmail.com>
2025-12-05 13:33:55 -0800

    exported nonfeasible to txt
    Identified which websites had null response for last-modified http and outputted those to separate txt file

--- scripts/discoverAndExtract.ts
+++ scripts/discoverAndExtract.ts
@@ -1,34 +1,41 @@
-import { readFile } from "fs/promises";
+import { readFile } from 'fs/promises';
+import { appendFile } from 'fs/promises';
 
 async function getLastedModified(url: string): Promise<string | null> {
+  const invalidFile = 'websiteModifiedNotFound.txt';
   try {
     let res = await fetch(url, {
-      method: "HEAD",
-      redirect: "follow"
+      method: 'HEAD',
+      redirect: 'follow',
     });
 
-    if (!res.ok || !res.headers.get("Last-Modified")) {
+    if (!res.ok || !res.headers.get('Last-Modified')) {
       res = await fetch(url, {
-        method: "GET",
-        redirect: "follow"
+        method: 'GET',
+        redirect: 'follow',
       });
     }
 
-    return res.headers.get("Last-Modified");
-  }
-  catch (err) {
+    if(res.headers.get('Last-Modified') == null) {
+      appendFile(invalidFile, (url + "\n"));
+    }
+    return res.headers.get('Last-Modified');
+  } catch (err) {
     return null;
   }
 }
 
 async function run() {
-  const text = await readFile("websites.txt", "utf8");
-  const urls = text.split("\n").map(x => x.trim()).filter(Boolean);
+  const text = await readFile('websites.txt', 'utf8');
+  const urls = text
+    .split('\n')
+    .map((x) => x.trim())
+    .filter(Boolean);
 
   for (const url of urls) {
     const lastMod = await getLastedModified(url);
-    console.log(`${url} -> ${lastMod}`)
+    console.log(`${url} -> ${lastMod}`);
   }
 }
 
-run()
+run();
